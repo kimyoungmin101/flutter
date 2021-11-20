bc5a0d68fe0e9398342b30c67aea0a93e9512d56
dudals2467 <dudals2467@kookmin.ac.kr>
2021-11-20 12:22:57 +0900

    rere

---
+++ memo_app/lib/models/memo_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Memo{
+  DateTime dateCreated;
+  String title;
+  String content;
+
+  Memo({required this.dateCreated, required this.title, required this.content});
+
+}
+
